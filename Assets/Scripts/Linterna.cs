8feef79f6f4255b91a860c489b69a3806038d749
AlexF093 <145749240+AlexF093@users.noreply.github.com>
2024-04-19 10:01:43 +0200

    clase

---
+++ Assets/Scripts/Linterna.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Linterna : MonoBehaviour
+{
+    // El objeto al que va a seguir la cámara
+    [SerializeField]
+    private Transform target;
+
+    // Ángulo de la cámara
+    [SerializeField]
+    private float angle;
+
+    // Distancia a la que se coloca la cámara con respecto a la arquera
+    [SerializeField]
+    private float distance;
+
+    // Desplazamiento con respecto al pivote de la arquera 
+    // (para que la cámara esté más orienta hacia la cabeza que a los pies)
+    [SerializeField]
+    private Vector3 offset;
+
+    // Velocidad a la que se mueve la cámara con Vector3.MoveTowards()
+    //[SerializeField]
+    //private float travelSpeed;
+
+    // Tiempo que tarda la cámara en moverse a la nueva ubicación con Vector3.Lerp()
+    [SerializeField]
+    private float travelTime;
+
+    private void Update()
+    {
+        /*Vector3 desiredPosition = target.transform.position + (Quaternion.Euler(0, angle, 0) * offset) - (target.forward * distance);
+        transform.position = Vector3.Lerp(transform.position, desiredPosition, travelTime / Time.deltaTime);
+        transform.LookAt(target.position + offset);*/
+        transform.LookAt(target.forward);
+        transform.position = target.position - target.forward * distance + offset;
+    }
+}
